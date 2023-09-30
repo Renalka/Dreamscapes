63c882b7156c1b3e2e64930f518572f9a4e56d6f
Renalka <rini.renalka.best@gmail.com>
2023-09-30 13:29:20 -0500

    made the dream journal searchable

--- app.py
+++ app.py
@@ -40,85 +40,88 @@ def process_input():
     try:
         action = request.form['action']
         dream_input = request.form['dream_input']
-        #print(dream_input)
-        # Saving dream to the database
-        new_dream = Dream(content=dream_input)
-        db.session.add(new_dream)
-        db.session.commit()
-        #print(new_dream)
-        
-        if action == 'generate':
-            # Generating dream description using OpenAI's GPT-4
-            response = openai.ChatCompletion.create(
-            model="gpt-4",
-            temperature=0,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a highly skilled AI trained in language comprehension. I would like you to read the given text, expand upon it and turn it into a storyline of one paragraph."
-                },
-                {
-                    "role": "user",
-                    "content": dream_input
-                }
-            ])
-            dream_description= response['choices'][0]['message']['content']
-
-            # Generating image
-            generated_image_url = generate_image(dream_input)
+        if dream_input!="":
+            #print(dream_input)
+            # Saving dream to the database
+            new_dream = Dream(content=dream_input)
+            db.session.add(new_dream)
+            db.session.commit()
+            #print(new_dream)
             
-            return render_template('dream.html', dream=dream_description, image_url=generated_image_url)
+            if action == 'generate':
+                # Generating dream description using OpenAI's GPT-4
+                response = openai.ChatCompletion.create(
+                model="gpt-4",
+                temperature=0,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are a highly skilled AI trained in language comprehension. I would like you to read the given text, expand upon it and turn it into a storyline of one paragraph."
+                    },
+                    {
+                        "role": "user",
+                        "content": dream_input
+                    }
+                ])
+                dream_description= response['choices'][0]['message']['content']
 
-        elif action == 'interpret':
-            # Interpreting dream using OpenAI's GPT-4
-            response = openai.ChatCompletion.create(
-            model="gpt-4",
-            temperature=0,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Act as a psychologist would and interpret this dream. Make sure to talk about the current state of mind the person seems to be in."
-                },
-                {
-                    "role": "user",
-                    "content": dream_input
-                }
-            ])
-            interpretation= response['choices'][0]['message']['content']
+                # Generating image
+                generated_image_url = generate_image(dream_input)
+                
+                return render_template('dream.html', dream=dream_description, image_url=generated_image_url)
 
-            return render_template('dream_interpretation.html', interpretation=interpretation)
+            elif action == 'interpret':
+                # Interpreting dream using OpenAI's GPT-4
+                response = openai.ChatCompletion.create(
+                model="gpt-4",
+                temperature=0,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "Act as a psychologist would and interpret this dream. Make sure to talk about the current state of mind the person seems to be in."
+                    },
+                    {
+                        "role": "user",
+                        "content": dream_input
+                    }
+                ])
+                interpretation= response['choices'][0]['message']['content']
+
+                return render_template('dream_interpretation.html', interpretation=interpretation)
+                
+            elif action == 'analyze':
+                response = openai.ChatCompletion.create(
+                model="gpt-4",
+                temperature=0,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "Perform detailed sentiment analysis in a few sentences of the given text."
+                    },
+                    {
+                        "role": "user",
+                        "content": dream_input
+                    }
+                ])
+                
+                sentiment= response['choices'][0]['message']['content']
+                # Using HuggingFace's pretrianed model to perform sentiment analysis
+                sentiment_result = nlp(dream_input)[0]
+                sentiment_label = sentiment_result['label']
+                sentiment_score = sentiment_result['score']
+
+                #print(response)
+                #return jsonify({'sentiment': sentiment, 'sentiment_label': sentiment_label, 'sentiment_score': sentiment_result['score']})
+                return render_template('dream_sentiment.html', sentiment_label=sentiment_label, sentiment_score=sentiment_score, sentiment=sentiment)
             
-        elif action == 'analyze':
-            response = openai.ChatCompletion.create(
-            model="gpt-4",
-            temperature=0,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Perform detailed sentiment analysis in a few sentences of the given text."
-                },
-                {
-                    "role": "user",
-                    "content": dream_input
-                }
-            ])
-            
-            sentiment= response['choices'][0]['message']['content']
-            # Using HuggingFace's pretrianed model to perform sentiment analysis
-            sentiment_result = nlp(dream_input)[0]
-            sentiment_label = sentiment_result['label']
-            sentiment_score = sentiment_result['score']
-
-            #print(response)
-            #return jsonify({'sentiment': sentiment, 'sentiment_label': sentiment_label, 'sentiment_score': sentiment_result['score']})
-            return render_template('dream_sentiment.html', sentiment_label=sentiment_label, sentiment_score=sentiment_score, sentiment=sentiment)
-        
-        elif action == 'recommend':
-            # Recommending books based on dream of the user
-            books = recommend_books(dream_input)
-            # print(recommended_books)
-            recommended_books = [f"{title} by {author}" for title, author in books]
-            return render_template('recommended_books.html', recommended_books=recommended_books)
+            elif action == 'recommend':
+                # Recommending books based on dream of the user
+                books = recommend_books(dream_input)
+                # print(recommended_books)
+                recommended_books = [f"{title} by {author}" for title, author in books]
+                return render_template('recommended_books.html', recommended_books=recommended_books)
+        else:
+            raise Exception("The field cannot be blank!")
             
     except Exception as e:
         return f"An error occurred: {e}", 500
@@ -137,7 +140,7 @@ def generate_image(dream_input):
 def dream_journal():
     if request.method == 'POST':
         search_term = request.form['search_term']
-        dreams = Dream.query.filter(Dream.description.like(f'%{search_term}%')).all()
+        dreams = Dream.query.filter(Dream.content.like(f'%{search_term}%')).all()
     else:
         dreams = Dream.query.all()
     
